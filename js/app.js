c3a4dd196a8ea39131b9dd16214dd6f4f36095e2
Aysheh Mahfouz <ayoosh.dub@hotmail.com>
2021-04-06 08:27:17 +0300

    finishing

---
+++ js/app.js
@@ -0,0 +1,91 @@
+'use strict';
+
+alert('WELCOME');
+
+
+
+let userName = prompt('what is your name?');
+userName = userName.toUpperCase();
+console.log(userName);
+alert('welcome to my page ' + userName);
+
+
+let school = prompt('am i ready to be a developer?');
+switch (school.toLowerCase()) {
+case 'yes':
+case 'y':
+case 'ys':
+  alert('totally');
+  break;
+case 'no':
+case 'n':
+  alert('thnx, will look into that');
+  break;
+
+}
+
+
+let view= prompt('are you a TA?');
+switch (view.toLowerCase()) {
+case 'yes':
+case 'y':
+case 'ys':
+  alert('welcome ');
+  break;
+case 'no':
+case 'n':
+  alert('welcome');
+  break;
+
+}
+
+let music= prompt('do i like music?');
+switch (music.toLowerCase()) {
+case 'yes':
+case 'y':
+case 'ys':
+  alert('yup and so should you ');
+  break;
+case 'no':
+case 'n':
+  alert('the exact opposite');
+  break;
+
+}
+
+let myName= prompt('do you know my nickname?');
+switch (myName.toLowerCase()) {
+case 'yes':
+case 'y':
+case 'ys':
+  alert('n!!!ce ');
+  break;
+case 'no':
+case 'n':
+  alert('ask me');
+  console.log(alert + 'myslack');
+  break;
+
+}
+
+let travel = prompt('have i travelled before?');
+switch (travel.toLowerCase()) {
+case 'yes':
+case 'y':
+case 'ys':
+  alert('and will always do');
+  break;
+case 'no':
+case 'n':
+  alert('actually did');
+  break;
+
+}
+
+
+
+
+
+
+
+
