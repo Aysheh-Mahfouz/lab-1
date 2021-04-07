b0243e687c7eae59fadaebe9da7a0050b994f54a
abdallakhattab <khattababdalla@yahoo.com>
2021-04-07 17:31:23 +0300

    added fun1

--- js/app.js
+++ js/app.js
@@ -12,133 +12,133 @@ alert('welcome to my page ' + userName);
 
 let school = prompt('am i ready to be a developer?');
 switch (school.toLowerCase()) {
-case 'yes':
-case 'y':
-case 'ys':
-  alert('totally');
-  score++;
-  break;
-case 'no':
-case 'n':
-  alert('thnx, will look into that');
-  break;
+  case 'yes':
+  case 'y':
+  case 'ys':
+    alert('totally');
+    score++;
+    break;
+  case 'no':
+  case 'n':
+    alert('thnx, will look into that');
+    break;
 
 }
 
 
 let view = prompt('are you a TA?');
 switch (view.toLowerCase()) {
-case 'yes':
-case 'y':
-case 'ys':
-  alert('welcome ');
-  score++;
-  break;
-case 'no':
-case 'n':
-  alert('welcome');
-  score++;
-  break;
+  case 'yes':
+  case 'y':
+  case 'ys':
+    alert('welcome ');
+    score++;
+    break;
+  case 'no':
+  case 'n':
+    alert('welcome');
+    score++;
+    break;
 
 }
 
 let music = prompt('do i like music?');
 switch (music.toLowerCase()) {
-case 'yes':
-case 'y':
-case 'ys':
-  alert('yup and so should you ');
-  score++;
-  break;
-case 'no':
-case 'n':
-  alert('the exact opposite');
-  break;
+  case 'yes':
+  case 'y':
+  case 'ys':
+    alert('yup and so should you ');
+    score++;
+    break;
+  case 'no':
+  case 'n':
+    alert('the exact opposite');
+    break;
 
 }
 
 let myName = prompt('do you know my nickname?');
 switch (myName.toLowerCase()) {
-case 'yes':
-case 'y':
-case 'ys':
-  score++;
-  alert('n!!!ce ');
-  break;
-case 'no':
-case 'n':
-  alert('ask me');
-  console.log(alert + 'myslack');
-  break;
+  case 'yes':
+  case 'y':
+  case 'ys':
+    score++;
+    alert('n!!!ce ');
+    break;
+  case 'no':
+  case 'n':
+    alert('ask me');
+    console.log(alert + 'myslack');
+    break;
 
 }
 
 let travel = prompt('have i travelled before?');
 switch (travel.toLowerCase()) {
-case 'yes':
-case 'y':
-case 'ys':
-  alert('and will always do');
-  score++;
-  break;
-case 'no':
-case 'n':
-  alert('actually did');
-  break;
+  case 'yes':
+  case 'y':
+  case 'ys':
+    alert('and will always do');
+    score++;
+    break;
+  case 'no':
+  case 'n':
+    alert('actually did');
+    break;
 
 }
 
 
+function question6() {
+  
 
+let userNumber = 10;
+let attempts = 5;
+let count = 3;
+let number = 7;
 
-function guessNum() {
-  let userNumber = 10;
-  let attempts = 5;
-  let count = 0;
-  let number = 7;
-  number = parseInt(number);
-  // the user must pick #7
+// the user must pick #7
 
-  let picks = prompt(' I am thinking of a Number between 1 & ' + userNumber);
+let picks = prompt(' I am thinking of a Number between 1 & ' + userNumber);
+picks = parseInt(picks);
+while (picks !== number) {
 
-  while (picks !== number) {
+  let picks = prompt(' pick a number between 1 and ' + userNumber);
+  count++;
+  picks = parseInt(picks);
+  if (count > attempts) {
+    alert(' the correct answer is ' + number);
+    break;
+  }
 
-    let picks = prompt(' pick a number between 1 and ' + userNumber);
-    count += 1;
+  if (picks === number) {
+    alert(' Lucky NUM 7! ' );
+    score++;
+    break;
+  }
 
-    if (count > attempts) {
-      alert(' the correct answer is ' + number);
-      break;
-    }
+  if (picks > number) {
+    alert('too high');
+  }
 
-    if (picks === number) {
-      alert(' Lucky NUM 7! ' + score++);
-      break;
-    }
+  if (picks < number) {
+    alert('too low');
+  }
 
-    if (picks > number) {
-      alert('too high');
-    }
-
-    if (picks < number) {
-      alert('too low');
-    }
-
-  } return picks;
 }
-guessNum();
+}
+question6()
 
 
 
-
-function guesscolor(){
+function guesscolor() {
   let colors = [
-    [ 'whats is my FAV color?',
+    ['whats is my FAV color?',
       'red',
       'white',
       'black']
   ];
-  for (let i  = 0;i < colors.length; i++) {
+  for (let i = 0; i < colors.length; i++) {
 
     let color = colors[i];
 
@@ -146,52 +146,52 @@ function guesscolor(){
 
     let c = 0;
 
-    let red  =color[1];
+    let red = color[1];
 
-    let white =color[2];
+    let white = color[2];
 
-    let black=color[3];
-    let pick='';
+    let black = color[3];
+    let pick = '';
 
     while (pick !== colors[i]) {
 
-      let pick= prompt(colors[0]);
+      let pick = prompt(colors[0]);
       pick = pick.toLowerCase();
       c += 1;
 
       if (c > attempts) {
-        alert('<p>'+' the correct answers are '+ color[1]+' ,' +color[2]+' ,'+color[3]+'</p');
+        alert('<p>' + ' the correct answers are ' + color[1] + ' ,' + color[2] + ' ,' + color[3] + '</p');
         break;
       }
-      if (pick ===red) {
+      if (pick === red) {
         score++;
         alert(' not that much but still will get a point' + ' your score is ' + score);
         break;
       }
 
-      if (pick ===white) {
+      if (pick === white) {
         score++;
-        alert(' queen of the colors ' + ' your score is ' +score);
+        alert(' queen of the colors ' + ' your score is ' + score);
         break;
       }
-      if (pick ===black) {
+      if (pick === black) {
         score++;
         alert(' king of the colors ' + ' your score is ' + score);
         break;
 
       }
-      if (pick!==red && pick!==white && pick!==black) {
+      if (pick !== red && pick !== white && pick !== black) {
         alert('wrong answer');
       }
     }
 
-  }return colors;
+  } return colors;
 }
 
 guesscolor();
 
 
 
-alert('your score is'+' ' +score+'/7');
+alert('your score is' + ' ' + score + '/7');
 
 alert('Thnxx 4 taking the time to visit my website ' + userName);
